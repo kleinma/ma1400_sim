cac0c281f5dfba033d22915153e77f7fcbda9fce
Matthew A Klein <gmattklein@gmail.com>
2017-06-05 13:49:27 -0400

    Changed to control muffler.

--- scripts/position_control.py
+++ scripts/position_control.py
@@ -28,40 +28,15 @@ import math
 
 if __name__ == "__main__":
   rospy.init_node('position_control')
-  base2s_pub = rospy.Publisher('/ma1400/base2s_controller/command', Float64,
-                               queue_size=1)
-  s2l_pub = rospy.Publisher('/ma1400/s2l_controller/command', Float64,
-                               queue_size=1)
-  l2u_pub = rospy.Publisher('/ma1400/l2u_controller/command', Float64,
-                               queue_size=1)
-  u2r_pub = rospy.Publisher('/ma1400/u2r_controller/command', Float64,
-                               queue_size=1)
-  r2b_pub = rospy.Publisher('/ma1400/r2b_controller/command', Float64,
-                               queue_size=1)
-  b2t_pub = rospy.Publisher('/ma1400/b2t_controller/command', Float64,
-                               queue_size=1)
-  base2s_pos = Float64()
-  s2l_pos = Float64()
-  l2u_pos = Float64()
-  u2r_pos = Float64()
-  r2b_pos = Float64()
-  b2t_pos = Float64()
+  muffler_pub = rospy.Publisher('/muffler_00/joint_revolute_controller/command',Float64,queue_size=1)
+
+  muffler_pos = Float64()
 
   rate = rospy.Rate(10) # 10hz
 
   while not rospy.is_shutdown():
     t = rospy.get_time()
-    base2s_pos.data = 0.1*math.sin(t)
-    s2l_pos.data = 0.1*math.sin(t)
-    l2u_pos.data = 0.1*math.sin(t)
-    u2r_pos.data = 0.1*math.sin(t)
-    r2b_pos.data = 0.1*math.sin(t)
-    b2t_pos.data = 0.1*math.sin(t)
+    muffler_pos.data = 2*math.pi*math.sin(t)
 
-    base2s_pub.publish(base2s_pos)
-    s2l_pub.publish(s2l_pos)
-    l2u_pub.publish(l2u_pos)
-    u2r_pub.publish(u2r_pos)
-    r2b_pub.publish(r2b_pos)
-    b2t_pub.publish(b2t_pos)
+    muffler_pub.publish(muffler_pos)
     rate.sleep()
